f5be1da2533e814f2b8b65eb25278cb7c54dbf04
Turkiano <turki.saeed.ksa@gmail.com>
2024-03-19 21:51:19 +0300

    only left with the validation

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Xml.XPath;
+
+Console.WriteLine("Type the tempature and its unit (C or F)");
+string? input = Console.ReadLine();
+
+
+
+while (true)
+
+
+{
+
+    if (input == "" || input is null || input.ToLower() == "exit")
+    {
+
+        Console.WriteLine("Terminated");
+        break;
+    }
+
+    int temp = int.Parse(input.Split()[0]);
+    char unit = Convert.ToChar(input.Split()[1]);
+
+
+    // Console.WriteLine($"TEMP ={temp}");
+    // Console.WriteLine($"UNIT ={unit}");
+
+
+    TempConverter.Converter(temp, unit);
+    break;
+}
+
+class TempConverter
+{
+    private string _temp;
+
+    public static void Converter(int temp, char unit)
+    {
+        Console.WriteLine($"TEMP ={temp}");
+        Console.WriteLine($"UNIT ={unit}");
+
+        double result = 0;
+      
+        {
+            result = (temp - 32) * (5.0 / 9.0);
+        }
+
+        Console.WriteLine($"RESULT {result}");
+    }
+
+
+
+
+
+}
